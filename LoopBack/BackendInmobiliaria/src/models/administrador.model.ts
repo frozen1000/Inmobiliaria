48502ce1c31aa102f0818ddf626e52aec39a9ce1
Yorman Pabon <yormanqp@gmail.com>
2022-10-24 22:38:24 -0500

    Creacion Modelo Administrador

---
+++ LoopBack/BackendInmobiliaria/src/models/administrador.model.ts
@@ -0,0 +1,64 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Administrador extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  apellidos: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  e-mail: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  clave: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  cedula: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  ciudad: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  celular: string;
+
+
+  constructor(data?: Partial<Administrador>) {
+    super(data);
+  }
+}
+
+export interface AdministradorRelations {
+  // describe navigational properties here
+}
+
+export type AdministradorWithRelations = Administrador & AdministradorRelations;
