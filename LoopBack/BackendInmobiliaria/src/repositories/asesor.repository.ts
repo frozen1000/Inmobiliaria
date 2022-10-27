30369ac19cc410b6657db998fc5664413dcbf571
Yorman Pabon <yormanqp@gmail.com>
2022-10-27 14:30:55 -0500

    Actualizacion Repos

--- LoopBack/BackendInmobiliaria/src/repositories/asesor.repository.ts
+++ LoopBack/BackendInmobiliaria/src/repositories/asesor.repository.ts
@@ -1,16 +1,27 @@
-import {inject} from '@loopback/core';
-import {DefaultCrudRepository} from '@loopback/repository';
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, HasManyRepositoryFactory} from '@loopback/repository';
 import {MongodbDataSource} from '../datasources';
-import {Asesor, AsesorRelations} from '../models';
+import {Asesor, AsesorRelations, Solicitud, Inmueble} from '../models';
+import {SolicitudRepository} from './solicitud.repository';
+import {InmuebleRepository} from './inmueble.repository';
 
 export class AsesorRepository extends DefaultCrudRepository<
   Asesor,
   typeof Asesor.prototype.id,
   AsesorRelations
 > {
+
+  public readonly solicituds: HasManyRepositoryFactory<Solicitud, typeof Asesor.prototype.id>;
+
+  public readonly inmuebles: HasManyRepositoryFactory<Inmueble, typeof Asesor.prototype.id>;
+
   constructor(
-    @inject('datasources.mongodb') dataSource: MongodbDataSource,
+    @inject('datasources.mongodb') dataSource: MongodbDataSource, @repository.getter('SolicitudRepository') protected solicitudRepositoryGetter: Getter<SolicitudRepository>, @repository.getter('InmuebleRepository') protected inmuebleRepositoryGetter: Getter<InmuebleRepository>,
   ) {
     super(Asesor, dataSource);
+    this.inmuebles = this.createHasManyRepositoryFactoryFor('inmuebles', inmuebleRepositoryGetter,);
+    this.registerInclusionResolver('inmuebles', this.inmuebles.inclusionResolver);
+    this.solicituds = this.createHasManyRepositoryFactoryFor('solicituds', solicitudRepositoryGetter,);
+    this.registerInclusionResolver('solicituds', this.solicituds.inclusionResolver);
   }
 }
--- LoopBack/BackendInmobiliaria/src/repositories/solicitud.repository.ts
+++ LoopBack/BackendInmobiliaria/src/repositories/solicitud.repository.ts
@@ -1,16 +1,22 @@
-import {inject} from '@loopback/core';
-import {DefaultCrudRepository} from '@loopback/repository';
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, HasOneRepositoryFactory} from '@loopback/repository';
 import {MongodbDataSource} from '../datasources';
-import {Solicitud, SolicitudRelations} from '../models';
+import {Solicitud, SolicitudRelations, Inmueble} from '../models';
+import {InmuebleRepository} from './inmueble.repository';
 
 export class SolicitudRepository extends DefaultCrudRepository<
   Solicitud,
   typeof Solicitud.prototype.id,
   SolicitudRelations
 > {
+
+  public readonly inmueble: HasOneRepositoryFactory<Inmueble, typeof Solicitud.prototype.id>;
+
   constructor(
-    @inject('datasources.mongodb') dataSource: MongodbDataSource,
+    @inject('datasources.mongodb') dataSource: MongodbDataSource, @repository.getter('InmuebleRepository') protected inmuebleRepositoryGetter: Getter<InmuebleRepository>,
   ) {
     super(Solicitud, dataSource);
+    this.inmueble = this.createHasOneRepositoryFactoryFor('inmueble', inmuebleRepositoryGetter);
+    this.registerInclusionResolver('inmueble', this.inmueble.inclusionResolver);
   }
 }
